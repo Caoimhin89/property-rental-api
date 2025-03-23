8e8eca5d98420f564c113652633e3404c672a0c2
Caoimhin89 <kevin.n.glick@gmail.com>
2025-03-23 14:17:08 +0500

    create kafka module

---
+++ src/kafka/kafka.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: process.env.KAFKA_CLIENT_ID || 'rental-app',
+            brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
+          },
+          consumer: {
+            groupId: process.env.KAFKA_CONSUMER_GROUP_ID || 'rental-consumer-group',
+          }
+        }
+      }
+    ])
+  ],
+  exports: [ClientsModule]
+})
+export class KafkaModule {} 
