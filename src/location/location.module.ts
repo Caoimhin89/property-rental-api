411bc503263a73a7478c4a6cd24e5206b54f031d
Caoimhin89 <kevin.n.glick@gmail.com>
2025-03-26 11:25:32 +0500

    register geocode service with location module

--- src/location/location.module.ts
+++ src/location/location.module.ts
@@ -2,11 +2,13 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Location } from './entities/location.entity';
 import { LocationService } from './location.service';
+import { GeocodingService } from './services/geocoding.service';
 import { LocationResolver } from './location.resolver';
 import { NearbyPlaceService } from '../nearby-place/nearby-place.service';
 import { NearbyPlace } from '../nearby-place/entities/nearby-place.entity';
 import { CacheModule } from '../cache/cache.module';
 import { CommonModule } from '../common/common.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     TypeOrmModule.forFeature([
@@ -14,16 +16,19 @@ import { CommonModule } from '../common/common.module';
       NearbyPlace
     ]),
     forwardRef(() => CacheModule),
-    CommonModule
+    CommonModule,
+    ConfigModule
   ],
   providers: [
-    LocationService, 
+    LocationService,
     LocationResolver,
-    NearbyPlaceService
+    NearbyPlaceService,
+    GeocodingService
   ],
   exports: [
     LocationService, 
-    LocationResolver
+    LocationResolver,
+    GeocodingService
   ]
 })
 export class LocationModule {} 
