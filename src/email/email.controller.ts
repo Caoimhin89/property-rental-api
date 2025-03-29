a29f80c1f4815a843b76975e7a70d3be48c9a3b2
Caoimhin89 <kevin.n.glick@gmail.com>
2025-03-29 22:46:47 +0500

    add additional event handlers

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -19,6 +19,24 @@ export class EmailController {
   @EventPattern('booking.created')
   async handleBookingCreated(@Payload() data: { email: string; bookingDetails: any }) {
     this.logger.debug('Received booking creation event', 'EmailController', data);
+    await this.emailService.sendBookingPendingConfirmation(data.email, data.bookingDetails);
+  }
+
+  @EventPattern('booking.confirmed')
+  async handleBookingConfirmed(@Payload() data: { email: string; bookingDetails: any }) {
+    this.logger.debug('Received booking confirmation event', 'EmailController', data);
     await this.emailService.sendBookingConfirmation(data.email, data.bookingDetails);
+  }
+
+  @EventPattern('booking.rejected')
+  async handleBookingRejected(@Payload() data: { email: string; bookingDetails: any }) {
+    this.logger.debug('Received booking rejection event', 'EmailController', data);
+    await this.emailService.sendBookingRejected(data.email, data.bookingDetails);
+  }
+
+  @EventPattern('booking.cancelled')
+  async handleBookingCancelled(@Payload() data: { email: string; bookingDetails: any }) {
+    this.logger.debug('Received booking cancellation event', 'EmailController', data);
+    await this.emailService.sendBookingCancelled(data.email, data.bookingDetails);
   }
 } 
